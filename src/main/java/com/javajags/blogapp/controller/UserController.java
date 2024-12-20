a64874478774b3c0b1467a43f8d41ed071aa2940
Jagdishchandradive <jagdishchandradive@gmail.com>
2024-12-20 10:54:30 +0530

    Optimized deleteUser API and added error handling

--- src/main/java/com/javajags/blogapp/controller/UserController.java
+++ src/main/java/com/javajags/blogapp/controller/UserController.java
@@ -1,9 +1,9 @@
 package com.javajags.blogapp.controller;
 
+import com.javajags.blogapp.exception.ResourceNotFoundException;
 import com.javajags.blogapp.payload.ApiResponse;
 import com.javajags.blogapp.payload.UserDto;
 import com.javajags.blogapp.service.UserServiceI;
-import com.javajags.blogapp.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,22 +23,30 @@ public class UserController {
         UserDto createUserDto = this.userServiceI.createUser(userDto);
         return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
     }
+
     @PutMapping("/{userId}")
     public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable Integer userId) {
         Optional<UserDto> updatedUser = this.userServiceI.updateUser(userDto, userId);
 
         return updatedUser.map(userDto1 -> new ResponseEntity<>(userDto1, HttpStatus.OK))
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("User", "ID", userId)
+                );
     }
-    @GetMapping("/")
+
+    @GetMapping("/get-all-users")
     public ResponseEntity<List<UserDto>> getAllUsers() {
         return ResponseEntity.ok(this.userServiceI.getAllUsers());
     }
 
     @DeleteMapping("/{userId}")
     public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userId") Integer uid) {
-        this.userServiceI.deleteUser(uid);
-        return new ResponseEntity<ApiResponse>(new ApiResponse("User deleted Successfully", true), HttpStatus.OK);
+        try {
+            this.userServiceI.deleteUser(uid);
+            return new ResponseEntity<ApiResponse>(new ApiResponse("User deleted Successfully", true), HttpStatus.OK);
+        } catch (ResourceNotFoundException e) {
+            return new ResponseEntity<>(new ApiResponse(e.getMessage(), false), HttpStatus.NOT_FOUND);
+        }
     }
 
 }
