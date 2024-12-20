8672a3516c64c0c15083b6c9a4e90e462b486097
Jagdishchandradive <jagdishchandradive@gmail.com>
2024-12-20 19:52:48 +0530

    Implemented logging in UserController to log user creation, update, deletion requests, and error handling

--- src/main/java/com/javajags/blogapp/controller/UserController.java
+++ src/main/java/com/javajags/blogapp/controller/UserController.java
@@ -4,6 +4,8 @@ import com.javajags.blogapp.exception.ResourceNotFoundException;
 import com.javajags.blogapp.payload.ApiResponse;
 import com.javajags.blogapp.payload.UserDto;
 import com.javajags.blogapp.service.UserServiceI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,36 +17,48 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserServiceI userServiceI;
 
     @PostMapping("/create-user")
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+        logger.info("Request received to create user: {}", userDto);
         UserDto createUserDto = this.userServiceI.createUser(userDto);
+        logger.info("User created successfully with ID: {}", createUserDto.getId());
         return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
     }
 
     @PutMapping("/{userId}")
     public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable Integer userId) {
+        logger.info("Request received to update user with ID: {}", userId);
         Optional<UserDto> updatedUser = this.userServiceI.updateUser(userDto, userId);
 
-        return updatedUser.map(userDto1 -> new ResponseEntity<>(userDto1, HttpStatus.OK))
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("User", "ID", userId)
-                );
+        return updatedUser.map(userDto1 -> {
+            logger.info("User updated successfully for ID: {}", userId);
+            return new ResponseEntity<>(userDto1, HttpStatus.OK);
+        }).orElseThrow(() -> {
+            logger.error("User not found with ID: {}", userId);
+            return new ResourceNotFoundException("User", "ID", userId);
+        });
     }
 
     @GetMapping("/get-all-users")
     public ResponseEntity<List<UserDto>> getAllUsers() {
+        logger.info("Request received to fetch all users");
         return ResponseEntity.ok(this.userServiceI.getAllUsers());
     }
 
+
     @DeleteMapping("/{userId}")
     public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userId") Integer uid) {
+        logger.info("Request received to delete user with ID: {}", uid);
         try {
             this.userServiceI.deleteUser(uid);
+            logger.info("User deleted successfully with  the ID: {}", uid);
             return new ResponseEntity<ApiResponse>(new ApiResponse("User deleted Successfully", true), HttpStatus.OK);
         } catch (ResourceNotFoundException e) {
+            logger.error("Error deleting user with ID: {}", uid, e);
             return new ResponseEntity<>(new ApiResponse(e.getMessage(), false), HttpStatus.NOT_FOUND);
         }
     }
